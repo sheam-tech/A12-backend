0d2f37cb08084d5725b25bd51c8a078a9d6f1c32
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-05-29 23:39:57 +0600

    mongo added

---
+++ index.js
@@ -0,0 +1,19 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
