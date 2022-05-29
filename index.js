2da8254f407eb6eff60dd39b418f10517fccb8ad
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-05-29 23:42:56 +0600

    remove admin

--- index.js
+++ index.js
@@ -35,4 +35,67 @@ const run = async () => {
       const reviewsCollection = db.collection("reviewsCollection");
       const blogsCollection = db.collection("blogsCollection");
       const adminsCollection = db.collection("adminsCollection");
+      app.put("/user/:email", async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        console.log("user", user);
+        const query = {
+          email: email
+        };
+        const options = {
+          upsert: true,
+        };
+        const updatedDoc = {
+          $set: {
+            email: user?.email,
+            role: user?.role,
+          },
+        };
+        const result = await usersCollection.updateOne(
+          query,
+          updatedDoc,
+          options
+        );
+        res.send(result);
+      });
+      //API to update a user
+      app.put("/update/user/:email", async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        console.log("user", user);
+        const query = {
+          email: email
+        };
+        const options = {
+          upsert: true,
+        };
+        const updatedDoc = {
+          $set: {
+            displayName: user?.displayName,
+            institution : user?.institution,
+            phoneNumber : user?.phoneNumber,
+            address : user?.address,
+            dateOfBirth : user?.dateOfBirth
+          },
+        };
+        const result = await usersCollection.updateOne(
+          query,
+          updatedDoc,
+          options
+        );
+        res.send(result);
+      });
   
+      //API to make Admin
+      app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
+        const email = req.params.email;
+        const filter = { email: email };
+        const options = { upsert: true }
+        const updateDoc = {
+          $set: { 
+            email: email,
+            role: "admin" },
+        };
+        const result = await adminsCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+      });
